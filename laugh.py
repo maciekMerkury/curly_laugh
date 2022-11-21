1175ebd600574e7949a78cd467de50eb6386f6c3
maciekMerkury <maciekmerkury2@gmail.com>
2022-11-21 01:31:12 +0100

    Added weighted random and limited output chars

--- laugh.py
+++ laugh.py
@@ -3,6 +3,26 @@
 from sys import argv
 from random import randint
 
+VALID_LETTERS = {
+    'S': 100,
+    'K': 100,
+    'D': 50,
+    'F': 50,
+}
+
+total_w = sum([VALID_LETTERS[k] for k in VALID_LETTERS])
+
+def int_to_weighted_char(i: int) -> chr:
+    for k in VALID_LETTERS:
+        if i <= VALID_LETTERS[k]:
+            return k
+        else:
+            i -= VALID_LETTERS[k]
+
+    print("how")
+    exit(1)
+
+
 if len(argv) == 1:
     print("need len")
     exit(1)
@@ -11,7 +31,7 @@ count = int(argv[1])
 
 str = ""
 for _ in range(count):
-    str += chr(randint(65, 90))
+    str += int_to_weighted_char(randint(0, total_w))
 
 print(str)
 
